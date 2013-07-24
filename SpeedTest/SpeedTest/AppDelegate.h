2933f4b73cf6b695312b604c8cc8855a52781376
Matthijs Hollemans <mail@hollance.com>
2013-07-24 17:09:32 +0200

    Added the test app back.

---
+++ SpeedTest/SpeedTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpeedTest
+//
+//  Created by Matthijs on 24-07-13.
+//  Copyright (c) 2013 Razeware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SpeedTest/SpeedTestTests/SpeedTestTests.m
@@ -0,0 +1,36 @@
+//
+//  SpeedTestTests.m
+//  SpeedTestTests
+//
+//  Created by Matthijs on 24-07-13.
+//  Copyright (c) 2013 Razeware. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SpeedTestTests : XCTestCase
+
+@end
+
+@implementation SpeedTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
