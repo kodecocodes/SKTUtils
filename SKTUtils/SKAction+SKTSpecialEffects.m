791304209749fa5f677e6ef235ca819fdd75e8bc
Matthijs Hollemans <mail@hollance.com>
2013-11-30 16:40:34 +0100

    Fixed formatting.

--- SKTUtils/SKAction+SKTSpecialEffects.m
+++ SKTUtils/SKAction+SKTSpecialEffects.m
@@ -25,52 +25,45 @@
 
 @implementation SKAction (SKTSpecialEffects)
 
-+ (instancetype)skt_screenShakeWithNode:(SKNode *)node amount:(CGPoint)amount oscillations:(int)oscillations duration:(NSTimeInterval)duration
-{
-CGPoint oldPosition = node.position;
-CGPoint newPosition = CGPointAdd(oldPosition, amount);
++ (instancetype)skt_screenShakeWithNode:(SKNode *)node amount:(CGPoint)amount oscillations:(int)oscillations duration:(NSTimeInterval)duration {
+  CGPoint oldPosition = node.position;
+  CGPoint newPosition = CGPointAdd(oldPosition, amount);
 
-SKTMoveEffect *effect = [[SKTMoveEffect alloc] initWithNode:node duration:duration startPosition:newPosition endPosition:oldPosition];
-effect.timingFunction = SKTCreateShakeFunction(oscillations);
+  SKTMoveEffect *effect = [[SKTMoveEffect alloc] initWithNode:node duration:duration startPosition:newPosition endPosition:oldPosition];
+  effect.timingFunction = SKTCreateShakeFunction(oscillations);
 
-return [SKAction actionWithEffect:effect];
+  return [SKAction actionWithEffect:effect];
 }
 
-+ (instancetype)skt_screenTumbleWithNode:(SKNode *)node angle:(CGFloat)angle oscillations:(int)oscillations duration:(NSTimeInterval)duration
-{
-CGFloat oldAngle = node.zRotation;
-CGFloat newAngle = oldAngle + angle;
++ (instancetype)skt_screenTumbleWithNode:(SKNode *)node angle:(CGFloat)angle oscillations:(int)oscillations duration:(NSTimeInterval)duration {
+  CGFloat oldAngle = node.zRotation;
+  CGFloat newAngle = oldAngle + angle;
 
-SKTRotateEffect *effect = [[SKTRotateEffect alloc] initWithNode:node duration:duration startAngle:newAngle endAngle:oldAngle];
-effect.timingFunction = SKTCreateShakeFunction(oscillations);
+  SKTRotateEffect *effect = [[SKTRotateEffect alloc] initWithNode:node duration:duration startAngle:newAngle endAngle:oldAngle];
+  effect.timingFunction = SKTCreateShakeFunction(oscillations);
 
-return [SKAction actionWithEffect:effect];
+  return [SKAction actionWithEffect:effect];
 }
 
-+ (instancetype)skt_screenZoomWithNode:(SKNode *)node amount:(CGPoint)amount oscillations:(int)oscillations duration:(NSTimeInterval)duration
-{
-CGPoint oldScale = CGPointMake(node.xScale, node.yScale);
-CGPoint newScale = CGPointMultiply(oldScale, amount);
++ (instancetype)skt_screenZoomWithNode:(SKNode *)node amount:(CGPoint)amount oscillations:(int)oscillations duration:(NSTimeInterval)duration {
+  CGPoint oldScale = CGPointMake(node.xScale, node.yScale);
+  CGPoint newScale = CGPointMultiply(oldScale, amount);
 
-SKTScaleEffect *effect = [[SKTScaleEffect alloc] initWithNode:node duration:duration startScale:newScale endScale:oldScale];
-effect.timingFunction = SKTCreateShakeFunction(oscillations);
+  SKTScaleEffect *effect = [[SKTScaleEffect alloc] initWithNode:node duration:duration startScale:newScale endScale:oldScale];
+  effect.timingFunction = SKTCreateShakeFunction(oscillations);
 
-return [SKAction actionWithEffect:effect];
+  return [SKAction actionWithEffect:effect];
 }
 
-+ (instancetype)skt_colorGlitchWithScene:(SKScene *)scene originalColor:(SKColor *)originalColor duration:(NSTimeInterval)duration
-{
-return [[self class] customActionWithDuration:duration actionBlock:^(SKNode *node, CGFloat elapsedTime)
-{
-if (elapsedTime < duration)
-{
-scene.backgroundColor = SKColorWithRGB(arc4random_uniform(255), arc4random_uniform(255), arc4random_uniform(255));
-}
-else
-{
-scene.backgroundColor = originalColor;
-}
-}];
++ (instancetype)skt_colorGlitchWithScene:(SKScene *)scene originalColor:(SKColor *)originalColor duration:(NSTimeInterval)duration {
+  return [[self class] customActionWithDuration:duration actionBlock:^(SKNode *node, CGFloat elapsedTime)
+  {
+    if (elapsedTime < duration) {
+      scene.backgroundColor = SKColorWithRGB(arc4random_uniform(255), arc4random_uniform(255), arc4random_uniform(255));
+    } else {
+      scene.backgroundColor = originalColor;
+    }
+  }];
 }
 
 @end
