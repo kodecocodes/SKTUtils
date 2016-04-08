5be5d9dc5ddb79d2ce6a87ed2b6aaba230e31174
softdevstory <developer@softdevstory.net>
2016-04-08 14:55:48 +0900

    SKTUtils.h is added.

---
+++ SKTUtils/SKTUtils.h
@@ -0,0 +1,19 @@
+//
+//  SKTUtils.h
+//  SKTUtils
+//
+//  Created by HS Song on 2016. 4. 8..
+//  Copyright © 2016년 raywenderlich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SKTUtils.
+FOUNDATION_EXPORT double SKTUtilsVersionNumber;
+
+//! Project version string for SKTUtils.
+FOUNDATION_EXPORT const unsigned char SKTUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SKTUtils/PublicHeader.h>
+
+
